4e48745038b1069639a0349bb812d4453407e11f
DIANA\Diana <dianbonilla@gmail.com>
2017-01-03 21:27:25 -0500

    deber estrella de n puntas

--- estrella n puntas.py
+++ estrella n puntas.py
@@ -13,24 +13,24 @@ if n % 2 == 0:
     a = 360/n 
     b = 360 - a
     t.right(45)
-    for x in range(0,n):
-        t.forward(100)
-        t.left(180-(360/n))
-##    for i in range(n):
+##    for x in range(0,n): ##sirve solo con 8 12 16 etc
 ##        t.forward(100)
-##        t.right(a)
-##        for i in range(3):
-##            t.color(0,0,1)
-##            t.begin_fill()
-##            t.forward(100)
-##            t.right(120)
-##            t.end_fill()
-##        for i in range(4):
-##            t.color(1,0,1)
-##            t.begin_fill()
-##            t.forward(100)
-##            t.right(90)
-##            t.end_fill()
+##        t.left(180-(360/n))
+    for i in range(n):
+        t.forward(100)
+        t.right(a)
+        for i in range(3):
+            t.color(0,0,1)
+            t.begin_fill()
+            t.forward(100)
+            t.right(120)
+            t.end_fill()
+        for i in range(4):
+            t.color(1,0,1)
+            t.begin_fill()
+            t.forward(100)
+            t.right(90)
+            t.end_fill()
 else:
     an = 180/n
     an2= 180-an
